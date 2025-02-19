c63d1a614317bd1546ec17d9d3fbd479cccd1059
Riad Mahi <riad.mahi@remedee.com>
2025-02-19 22:20:06 +0100

    Add details rating and video component to movie details screen

--- composeApp/src/commonMain/kotlin/com/riadmahi/movienow/ui/main/movieDetails/MovieDetailsScreen.kt
+++ composeApp/src/commonMain/kotlin/com/riadmahi/movienow/ui/main/movieDetails/MovieDetailsScreen.kt
@@ -1,9 +1,20 @@
 package com.riadmahi.movienow.ui.main.movieDetails
 
-import androidx.compose.material.Text
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import com.riadmahi.movienow.ui.main.movieDetails.components.MovieDetailsDescription
+import com.riadmahi.movienow.ui.main.movieDetails.components.MovieDetailsHeader
+import com.riadmahi.movienow.ui.main.movieDetails.components.MovieDetailsRatingAndVideo
 
 @Composable
 fun MovieDetailsScreen() {
-    Text("Hello movie screen")
+    LazyColumn(modifier = Modifier.fillMaxSize()) {
+        item {
+            MovieDetailsHeader()
+            MovieDetailsRatingAndVideo()
+            MovieDetailsDescription()
+        }
+    }
 }
