a843ec62aefaa8953a9f28e40c6640c2eab12cb9
Riad Mahi <riad.mahi@remedee.com>
2025-02-20 20:45:24 +0100

    Add movie param to carousel and add automatic sliding

--- composeApp/src/commonMain/kotlin/com/riadmahi/movienow/ui/common/Carousel.kt
+++ composeApp/src/commonMain/kotlin/com/riadmahi/movienow/ui/common/Carousel.kt
@@ -1,6 +1,7 @@
 package com.riadmahi.movienow.ui.common
 
 import androidx.compose.animation.Crossfade
+import androidx.compose.animation.core.tween
 import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.pager.HorizontalPager
@@ -8,9 +9,11 @@ import androidx.compose.foundation.pager.PagerState
 import androidx.compose.foundation.pager.rememberPagerState
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Card
+import androidx.compose.material.CircularProgressIndicator
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.blur
@@ -21,13 +24,14 @@ import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.unit.IntOffset
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.compose.ui.util.lerp
 import coil3.compose.AsyncImage
+import com.riadmahi.movienow.ui.main.explore.ExploreUiState
+import kotlinx.coroutines.delay
+import kotlinx.datetime.LocalDate
 import kotlin.math.absoluteValue
-import kotlin.math.roundToInt
 
 /**
  * Extension function to compute the current offset for a given page.
@@ -39,111 +43,143 @@ fun PagerState.calculateCurrentOffsetForPage(page: Int): Float {
 }
 
 @Composable
-fun Carousel() {
-    Column(modifier = Modifier.fillMaxSize()) {
-        // Replace these with your actual list of image URLs.
-        val sliderList = listOf(
-            "https://images.unsplash.com/photo-1739536176048-caa7190dba66?q=80&w=1974&auto=format&fit=crop",
-            "https://images.unsplash.com/photo-1739911636351-d1a19684de06?q=80&w=1976&auto=format&fit=crop",
-            "https://images.unsplash.com/photo-1593642532400-2682810df593?q=80&w=1974&auto=format&fit=crop"
-        )
-        // Create and remember the PagerState with the number of pages based on sliderList.
-        val pagerState = rememberPagerState(pageCount = { sliderList.size })
-        Box(
-            modifier = Modifier.fillMaxWidth().height(400.dp).clip(
-                shape = RoundedCornerShape(
-                    topEnd = 12.dp,
-                    topStart = 12.dp,
-                    bottomEnd = 0.dp,
-                    bottomStart = 0.dp
-                )
-            )
-        ) {
-            Crossfade(targetState = pagerState.currentPage) { currentPage ->
-                Box(modifier = Modifier.fillMaxSize()) {
-                    AsyncImage(
-                        model = sliderList[currentPage],
-                        contentDescription = null,
-                        modifier = Modifier
-                            .fillMaxSize()
-                            .blur(50.dp),
-                        contentScale = ContentScale.Crop
-                    )
-                    Canvas(
-                        modifier = Modifier
-                            .fillMaxWidth()
-                            .height(200.dp)
-                            .align(Alignment.BottomCenter),
-                        onDraw = {
-                            drawRect(
-                                brush = Brush.verticalGradient(
-                                    colors = listOf(Color.Transparent, Color.Black),
-                                    startY = 0.5f
-                                )
-                            )
-                        }
+fun Carousel(
+    state: ExploreUiState.MovieListState,
+) {
+
+    when (state) {
+        is ExploreUiState.MovieListState.Loading -> {
+            Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
+                CircularProgressIndicator()
+            }
+        }
+
+        is ExploreUiState.MovieListState.Error -> {
+            Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
+                Text(text = "Error: ${state.errorMessage}")
+            }
+        }
+
+        is ExploreUiState.MovieListState.Success -> {
+            val movies = state.movies
+            val pagerState = rememberPagerState(pageCount = { movies.size })
+            LaunchedEffect(pagerState) {
+                while (true) {
+                    delay(3000L)
+                    val nextPage = (pagerState.currentPage + 1) % movies.size
+                    pagerState.animateScrollToPage(
+                        nextPage,
+                        animationSpec = tween(durationMillis = 1400)
                     )
                 }
             }
-            HorizontalPager(
-                modifier = Modifier.fillMaxWidth().height(500.dp),
-                state = pagerState,
-                verticalAlignment = Alignment.CenterVertically,
-                pageSpacing = 100.dp
-            ) { page ->
-                Box(
-                    modifier = Modifier.fillMaxSize(),
-                    contentAlignment = Alignment.BottomCenter
-                ) {
-                    val pageOffset = pagerState.calculateCurrentOffsetForPage(page)
-                    Column(horizontalAlignment = Alignment.CenterHorizontally) {
-                        Card(
-                            modifier = Modifier.size(width = 220.dp, height = 320.dp).graphicsLayer {
-                                val scale = lerp(0.8f, 1f, 1f - pageOffset.absoluteValue)
-                                scaleX = scale
-                                scaleY = scale
-                                alpha = lerp(0.6f, 1f, 1f - pageOffset.absoluteValue)
-                                rotationX = lerp(10f, 0f, 1f - pageOffset.absoluteValue)
-                                translationY = lerp(60f, 0f, 1f - pageOffset.absoluteValue)
-                                rotationZ = lerp(15f, 0f, 1f - pageOffset.absoluteValue)
-                            },
-                            backgroundColor = Color.Transparent,
-                            shape = RoundedCornerShape(12.dp),
-                            elevation = 0.dp
-                        ) {
-                            AsyncImage(
-                                modifier = Modifier.fillMaxSize(),
-                                model = sliderList[page],
-                                contentDescription = null,
-                                contentScale = ContentScale.Crop
-                            )
-                        }
-                        Column(
-                            horizontalAlignment = Alignment.CenterHorizontally,
-                            modifier = Modifier.padding(top = 6.dp).graphicsLayer {
-                                val scale = lerp(0.5f, 1f, 1f - pageOffset.absoluteValue)
-                                scaleX = scale
-                                scaleY = scale
-                                alpha = lerp(0.6f, 1f, 1f - pageOffset.absoluteValue)
-                                rotationX = lerp(25f, 0f, 1f - pageOffset.absoluteValue)
-                                translationY = lerp(70f, 0f, 1f - pageOffset.absoluteValue)
+            Box(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(400.dp)
+                    .clip(
+                        shape = RoundedCornerShape(
+                            topEnd = 12.dp,
+                            topStart = 12.dp,
+                            bottomEnd = 0.dp,
+                            bottomStart = 0.dp
+                        )
+                    )
+            ) {
+                Crossfade(targetState = pagerState.currentPage) { currentPage ->
+                    Box(modifier = Modifier.fillMaxSize()) {
+                        AsyncImage(
+                            model = "https://image.tmdb.org/t/p/original/${movies[currentPage].backdropPath}",
+                            contentDescription = null,
+                            modifier = Modifier
+                                .fillMaxSize()
+                                .blur(50.dp),
+                            contentScale = ContentScale.Crop
+                        )
+                        Canvas(
+                            modifier = Modifier
+                                .fillMaxWidth()
+                                .height(200.dp)
+                                .align(Alignment.BottomCenter),
+                            onDraw = {
+                                drawRect(
+                                    brush = Brush.verticalGradient(
+                                        colors = listOf(Color.Transparent, Color.Black),
+                                        startY = 0f,
+                                        endY = size.height
+                                    )
+                                )
                             }
-                        ) {
-                            Text(
-                                text = "Titre",
-                                fontWeight = FontWeight.Medium,
-                                fontSize = 20.sp,
-                                color = MaterialTheme.colors.onSurface,
-                                textAlign = TextAlign.Center
-                            )
-
-                            Text(
-                                text = "2024",
-                                fontWeight = FontWeight.Normal,
-                                fontSize = 16.sp,
-                                color = Color.Gray,
-                                textAlign = TextAlign.Center
-                            )
+                        )
+                    }
+                }
+                // Horizontal pager displaying movie cards.
+                HorizontalPager(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .height(500.dp),
+                    state = pagerState,
+                    verticalAlignment = Alignment.CenterVertically,
+                    pageSpacing = 100.dp
+                ) { page ->
+                    Box(
+                        modifier = Modifier.fillMaxSize(),
+                        contentAlignment = Alignment.BottomCenter
+                    ) {
+                        val pageOffset = pagerState.calculateCurrentOffsetForPage(page)
+                        Column(horizontalAlignment = Alignment.CenterHorizontally) {
+                            Card(
+                                modifier = Modifier
+                                    .size(width = 220.dp, height = 320.dp)
+                                    .graphicsLayer {
+                                        val scale = lerp(0.8f, 1f, 1f - pageOffset.absoluteValue)
+                                        scaleX = scale
+                                        scaleY = scale
+                                        alpha = lerp(0.6f, 1f, 1f - pageOffset.absoluteValue)
+                                        rotationX = lerp(10f, 0f, 1f - pageOffset.absoluteValue)
+                                        translationY = lerp(60f, 0f, 1f - pageOffset.absoluteValue)
+                                        rotationZ = lerp(15f, 0f, 1f - pageOffset.absoluteValue)
+                                    },
+                                backgroundColor = Color.Transparent,
+                                shape = RoundedCornerShape(12.dp),
+                                elevation = 0.dp
+                            ) {
+                                AsyncImage(
+                                    modifier = Modifier.fillMaxSize(),
+                                    model = "https://image.tmdb.org/t/p/original/${movies[page].posterPath}",
+                                    contentDescription = null,
+                                    contentScale = ContentScale.Crop
+                                )
+                            }
+                            // Text info (title and release year)
+                            Column(
+                                horizontalAlignment = Alignment.CenterHorizontally,
+                                modifier = Modifier
+                                    .padding(top = 6.dp)
+                                    .graphicsLayer {
+                                        val scale = lerp(0.5f, 1f, 1f - pageOffset.absoluteValue)
+                                        scaleX = scale
+                                        scaleY = scale
+                                        alpha = lerp(0f, 1f, 1f - pageOffset.absoluteValue)
+                                        rotationX = lerp(25f, 0f, 1f - pageOffset.absoluteValue)
+                                        translationY = lerp(70f, 0f, 1f - pageOffset.absoluteValue)
+                                    }
+                            ) {
+                                Text(
+                                    text = movies[page].title,
+                                    fontWeight = FontWeight.Medium,
+                                    fontSize = 16.sp,
+                                    color = MaterialTheme.colors.onSurface,
+                                    textAlign = TextAlign.Center
+                                )
+                                Text(
+                                    text = "${movies[page].releaseDate.let { LocalDate.parse(it).year }}",
+                                    fontWeight = FontWeight.Normal,
+                                    fontSize = 14.sp,
+                                    color = Color.Gray,
+                                    textAlign = TextAlign.Center
+                                )
+                            }
                         }
                     }
                 }
