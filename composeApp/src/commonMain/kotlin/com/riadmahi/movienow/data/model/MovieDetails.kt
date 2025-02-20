0361010bccee071f0222f2e4f2a42d2687640534
Riad Mahi <riad.mahi@remedee.com>
2025-02-20 09:31:48 +0100

    Fix MovieDetails model

--- composeApp/src/commonMain/kotlin/com/riadmahi/movienow/data/model/MovieDetails.kt
+++ composeApp/src/commonMain/kotlin/com/riadmahi/movienow/data/model/MovieDetails.kt
@@ -1,58 +1,85 @@
 package com.riadmahi.movienow.data.model
 
+import kotlinx.serialization.Serializable
+import kotlinx.serialization.SerialName
+
+@Serializable
 data class MovieDetails(
     val adult: Boolean,
-    val backdrop_path: String?,
-    val belongs_to_collection: Collection?,
+    @SerialName("backdrop_path")
+    val backdropPath: String?,
+    @SerialName("belongs_to_collection")
+    val belongsToCollection: Collection?,
     val budget: Int,
     val genres: List<Genre>,
     val homepage: String?,
     val id: Int,
-    val imdb_id: String?,
-    val original_language: String,
-    val original_title: String,
+    @SerialName("imdb_id")
+    val imdbId: String?,
+    @SerialName("original_language")
+    val originalLanguage: String,
+    @SerialName("original_title")
+    val originalTitle: String,
     val overview: String?,
     val popularity: Double,
-    val poster_path: String?,
-    val production_companies: List<ProductionCompany>,
-    val production_countries: List<ProductionCountry>,
-    val release_date: String?,
+    @SerialName("poster_path")
+    val posterPath: String?,
+    @SerialName("production_companies")
+    val productionCompanies: List<ProductionCompany>,
+    @SerialName("production_countries")
+    val productionCountries: List<ProductionCountry>,
+    @SerialName("release_date")
+    val releaseDate: String?,
     val revenue: Int,
     val runtime: Int?,
-    val spoken_languages: List<SpokenLanguage>,
+    @SerialName("spoken_languages")
+    val spokenLanguages: List<SpokenLanguage>,
     val status: String,
     val tagline: String?,
     val title: String,
     val video: Boolean,
-    val vote_average: Double,
-    val vote_count: Int
+    @SerialName("vote_average")
+    val voteAverage: Double,
+    @SerialName("vote_count")
+    val voteCount: Int
 )
 
+@Serializable
 data class Collection(
     val id: Int,
     val name: String?,
-    val poster_path: String?,
-    val backdrop_path: String?
+    @SerialName("poster_path")
+    val posterPath: String?,
+    @SerialName("backdrop_path")
+    val backdropPath: String?
 )
 
+@Serializable
 data class Genre(
     val id: Int,
     val name: String
 )
 
+@Serializable
 data class ProductionCompany(
     val id: Int,
     val name: String,
-    val logo_path: String?,
-    val origin_country: String
+    @SerialName("logo_path")
+    val logoPath: String?,
+    @SerialName("origin_country")
+    val originCountry: String
 )
 
+@Serializable
 data class ProductionCountry(
-    val iso_3166_1: String,
+    @SerialName("iso_3166_1")
+    val iso31661: String,
     val name: String
 )
 
+@Serializable
 data class SpokenLanguage(
-    val iso_639_1: String,
+    @SerialName("iso_639_1")
+    val iso6391: String,
     val name: String
 )
