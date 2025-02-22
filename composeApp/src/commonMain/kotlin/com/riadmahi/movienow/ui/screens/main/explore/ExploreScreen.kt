3eb640aa8241efd690fa98f8e036000d73109153
Riad Mahi <riad.mahi@remedee.com>
2025-02-22 10:32:52 +0100

    Improve explore screen ui

--- composeApp/src/commonMain/kotlin/com/riadmahi/movienow/ui/screens/main/explore/ExploreScreen.kt
+++ composeApp/src/commonMain/kotlin/com/riadmahi/movienow/ui/screens/main/explore/ExploreScreen.kt
@@ -1,13 +1,16 @@
 package com.riadmahi.movienow.ui.main.explore
 
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.Canvas
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material.CircularProgressIndicator
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import com.riadmahi.movienow.data.model.Movie
@@ -27,31 +30,41 @@ fun ExploreScreen(
     onNavigateToMovieDetails: (Movie) -> Unit,
 ) {
     val uiState by viewModel.uiState.collectAsStateWithLifecycle()
-    LazyColumn(verticalArrangement = Arrangement.spacedBy(24.dp)) {
-        if (uiState is ExploreUiState.Content) {
-            item {
-                Carousel(
-                    state = (uiState as ExploreUiState.Content).upcomingMovies,
-                    onMovieClick = { onNavigateToMovieDetails(it) }
-                )
-                MovieSection(
-                    state = (uiState as ExploreUiState.Content).popularMovies,
-                    title = stringResource(Res.string.movie_section_popular),
-                    cardSize = CardSize.Medium,
-                    onMovieClick = { onNavigateToMovieDetails(it) }
-                )
-                MovieSection(
-                    state = (uiState as ExploreUiState.Content).nowPlayingMovies,
-                    title = stringResource(Res.string.movie_section_now_playing),
-                    onMovieClick = { onNavigateToMovieDetails(it) }
-                )
-                MovieSection(
-                    state = (uiState as ExploreUiState.Content).topRatedMovies,
-                    title = stringResource(Res.string.movie_section_top_rated),
-                    onMovieClick = { onNavigateToMovieDetails(it) }
-                )
+    Box(modifier = Modifier.fillMaxSize()) {
+        LazyColumn(verticalArrangement = Arrangement.spacedBy(24.dp)) {
+            if (uiState is ExploreUiState.Content) {
+                item {
+                    Carousel(
+                        state = (uiState as ExploreUiState.Content).upcomingMovies,
+                        onMovieClick = { onNavigateToMovieDetails(it) }
+                    )
+                    MovieSection(
+                        state = (uiState as ExploreUiState.Content).popularMovies,
+                        title = stringResource(Res.string.movie_section_popular),
+                        cardSize = CardSize.Medium,
+                        onMovieClick = { onNavigateToMovieDetails(it) }
+                    )
+                    MovieSection(
+                        state = (uiState as ExploreUiState.Content).nowPlayingMovies,
+                        title = stringResource(Res.string.movie_section_now_playing),
+                        onMovieClick = { onNavigateToMovieDetails(it) }
+                    )
+                    MovieSection(
+                        state = (uiState as ExploreUiState.Content).topRatedMovies,
+                        title = stringResource(Res.string.movie_section_top_rated),
+                        onMovieClick = { onNavigateToMovieDetails(it) }
+                    )
+                }
             }
         }
+        Canvas(
+            modifier = Modifier.fillMaxWidth().height(25.dp).align(Alignment.BottomCenter),
+            onDraw = {
+                drawRect(
+                    Brush.verticalGradient(listOf(Color.Transparent, Color.Black))
+                )
+            }
+        )
     }
 }
 
