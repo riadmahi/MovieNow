5de20db3441ab7b9d6e2197de53190c95ca69db3
Riad Mahi <riad.mahi@remedee.com>
2025-02-24 16:55:03 +0100

    Update movie preview model

--- composeApp/src/commonMain/kotlin/com/riadmahi/movienow/data/model/local/MoviePreview.kt
+++ composeApp/src/commonMain/kotlin/com/riadmahi/movienow/data/model/local/MoviePreview.kt
@@ -5,7 +5,8 @@ import androidx.room.PrimaryKey
 
 @Entity(tableName = "movie_preview")
 data class MoviePreview(
-    @PrimaryKey val id: Int,
+    @PrimaryKey
+    val id: Int,
     val title: String,
     val posterPath: String? = null
 )
